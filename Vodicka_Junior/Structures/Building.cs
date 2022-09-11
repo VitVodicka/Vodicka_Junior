ac12a2cf7955b33cbf40dfad8e67a074c58518a1
rogiave12 <63801689+rogiave12@users.noreply.github.com>
2022-09-11 05:50:04 +0200

    Tables created

---
+++ Vodicka_Junior/Structures/Building.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vodicka_Junior
+{
+    internal class Building
+    {
+        public int BuildingId { get; set; }
+
+        
+
+        public DateTime FirstApproval { get; set; }
+        public Building(DateTime firstApproval, int buildingId)
+        {
+            BuildingId = buildingId;
+            FirstApproval = firstApproval;
+        }
+
+    }
+}
